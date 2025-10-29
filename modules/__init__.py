3114d17c289289c73569c3dd1b2f6c9a18481cd4
Buildbetbigger <buildbetbigger@gmail.com>
2025-10-29 12:28:55 -0700

    Delete modules/__init__.py

--- modules/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Core modules for DFS Meta-Optimizer"""
-from .opponent_modeling import OpponentModel
-from .optimization_engine import LineupOptimizer
-
-try:
-    from .claude_assistant import ClaudeAssistant
-except ImportError:
-    ClaudeAssistant = None
